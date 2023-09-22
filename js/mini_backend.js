d2bb1417aa9933c769a897b6e7ec24dd438a4404
Elijah Degen <elidegen@icloud.com>
2023-09-22 22:11:01 +0200

    x

--- js/mini_backend.js
+++ js/mini_backend.js
@@ -19,16 +19,15 @@ const backendOld = {
 };
 
 const backend = {
-    setItem: function (key, value) {
+    setItem: async function (key, value) {
         const payload = { key, value, token: storageToken };
         return fetch(storageUrl, { method: 'POST', body: JSON.stringify(payload) })
             .then(res => res.json());
     },
 
-    getItem: function (key) {
+    getItem: async function (key) {
         const url = `${storageUrl}?key=${key}&token=${storageToken}`;
-        return fetch(url)
-            .then(res => res.json()).then(res => res.data.value);
+        return fetch(url).then(res => res.json()).then(res => res.data.value);
     }
 };
 
